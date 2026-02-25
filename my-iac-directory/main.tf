618b37b1d845ff939dfe66cbbc737e6c0e3c82cd
kofiade <83889198+kofiade@users.noreply.github.com>
2026-02-25 10:58:16 +0000

    Add infrastructure resources for S3 and security group
    Define S3 bucket, public access block, and security group.

---
+++ my-iac-directory/main.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "example" {
+  bucket = "my-test-bucket"
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_security_group" "open" {
+  name = "wide-open"
+
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
